04fda7e82ff2c9743ebc10a9bc614d60724b1bed
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2023-05-22 00:52:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express'); //requiring express
 const app = express(); //app has the properties of express
 const passport = require('passport'); //requiring passport
 // const jwtPassportStrategy = require('./config/passport_jwt'); //passport jwt config file
-// const db = require('./config/mongoose'); //mongoose connection
+
+// connect to the database mongoose connection
+const db = require('./config/mongoose');
+db();
+// load config from dot env file
 require('dotenv').config();
 const PORT = process.env.PORT || 8000; //port on which server runs
 
-// app.use(express.urlencoded({extended: true})); //to parse form data
-app.use('/', require('./routes/index')); //set up scalable routes folder
+//to parse form data
+app.use(express.urlencoded({extended: true}));
+// middleware to parse json request body
+app.use(express.json());
 
-//server listens on port
+//set up scalable routes
+app.use('/', require('./routes/index'));
+
+// start server
 app.listen(PORT, function (err) {
     if (err) {
         console.log('An error occured in running the server!');
