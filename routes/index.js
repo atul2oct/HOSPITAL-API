49b0199f884fdd7b4551e679d2c6f4c8e656724d
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2023-05-22 00:52:26 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    return res.json(400, {
-        message: 'Please request the correct routes!'
-    }
-)});
+// router.get('/', function(req, res) {
+//     return res.json(400, {
+//         message: 'Please request the correct routes!'
+//     }
+// )});
 
-router.use('/doctors', require('./doctor')); //routes to all doctors reuqest
-router.use('/patients', require('./patient')); //routes to all pateints request
-router.use('/reports', require('./report')); //routes to all reports request
+// router.use('/doctors', require('./doctor')); //routes to all doctors reuqest
+// router.use('/patients', require('./patient')); //routes to all pateints request
+// router.use('/reports', require('./report')); //routes to all reports request
+const Test=require('../controller/test');
+router.get('/',Test.Test);
 
 module.exports = router;
