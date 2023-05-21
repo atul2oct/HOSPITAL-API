f437294f18874c1f21d5c4b4ac2f7e58cfab55f3
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2023-05-22 00:58:00 +0530

    Create passport_jwt.js

---
+++ config/passport_jwt.js
@@ -0,0 +1,29 @@
+const passport = require('passport'); //requiring passport
+const JWTStrategy = require('passport-jwt').Strategy; //jwt strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt; //extracting the jwt token
+const Doctor = require('../models/doctor');
+
+require('dotenv').config();
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SESSION_SECRET_KEY, //secret key used to encrypt/decrypt
+}
+
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done) {
+    Doctor.findById(jwtPayLoad._id, function(err, doctor) {
+        if(err) {
+            //if error occured
+            console.log('Error in finding user --> Passport JWT');
+            return done(err);
+        }
+        if(doctor) {
+            //if doctor found
+            return done(null, doctor);
+        }else {
+            //if doctor not found
+            return done(null, false);
+        }
+    })
+}));
+
+module.exports = passport;
